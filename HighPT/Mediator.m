65794fffb4f0639330f33706520ce27b42af428a
Lukas Allwicher <lukall@physik.uzh.ch>
2022-04-04 17:44:27 +0200

    added mediator matching to FFs

--- HighPT/Mediator.m
+++ HighPT/Mediator.m
@@ -4,7 +4,7 @@ Package["HighPT`"]
 
 
 (* ::Title:: *)
-(*HighPTio`SMEFT`*)
+(*HighPTio`Mediator`*)
 
 
 (* ::Subtitle:: *)
@@ -72,7 +72,7 @@ WC[x_,{f1_[a_],f2_[b_]}]:= WC[x,{a,b}]*)
 
 $MediatorList= {
 	"Zp",
-	"Zt",
+	(*"Zt",*)
 	"Wp",
 	"S1",
 	"S1t",
@@ -89,7 +89,7 @@ $MediatorList= {
 
 $MediatorCouplings= <|
 	"Zp" -> {Coupling["g1u",{i,j}],Coupling["g1d",{i,j}],Coupling["g1e",{\[Alpha],\[Beta]}],Coupling["g1q",{i,j}],Coupling["g1l",{\[Alpha],\[Beta]}]},
-	"Zt" -> {Coupling["g1qt",{i,j}],Coupling["g1lt",{\[Alpha],\[Beta]}]},
+	(*"Zt" -> {Coupling["g1qt",{i,j}],Coupling["g1lt",{\[Alpha],\[Beta]}]},*)
 	"Wp" -> {Coupling["g3q",{i,j}],Coupling["g3l",{\[Alpha],\[Beta]}]},
 	"S1" -> {Coupling["y1L",{i,\[Alpha]}],Coupling["y1R",{i,\[Alpha]}],Coupling["y1Rb",{i,\[Alpha]}]},
 	"S1t" -> {Coupling["y1Rt",{i,\[Alpha]}]},
@@ -131,114 +131,147 @@ Coupling[herm:HermitianCouplings,{p_Integer,r_Integer}]:= Coupling[herm,{r,p}]\[
 SubstitutionRulesMediators["Zp"]:={
 	(* Vector *)
 	(* NC *)
-	FF[Vector, {"Zp",0}, {Left,Left},   {a_,b_,i_d,j_d}] :> Coupling["g1q",{i,j}]Coupling["g1l",{a,b}],
-	FF[Vector, {"Zp",0}, {Left,Right},  {a_,b_,i_d,j_d}] :> Coupling["g1d",{i,j}]Coupling["g1l",{a,b}],
-	FF[Vector, {"Zp",0}, {Right,Left},  {a_,b_,i_d,j_d}] :> Coupling["g1q",{i,j}]Coupling["g1e",{a,b}],
-	FF[Vector, {"Zp",0}, {Right,Right}, {a_,b_,i_d,j_d}] :> Coupling["g1d",{i,j}]Coupling["g1e",{a,b}],
 	FF[Vector, {"Zp",0}, {Left,Left},   {a_,b_,i_u,j_u}] :> Coupling["g1q",{i,j}]Coupling["g1l",{a,b}],
 	FF[Vector, {"Zp",0}, {Left,Right},  {a_,b_,i_u,j_u}] :> Coupling["g1u",{i,j}]Coupling["g1l",{a,b}],
 	FF[Vector, {"Zp",0}, {Right,Left},  {a_,b_,i_u,j_u}] :> Coupling["g1q",{i,j}]Coupling["g1e",{a,b}],
-	FF[Vector, {"Zp",0}, {Right,Right}, {a_,b_,i_u,j_u}] :> Coupling["g1u",{i,j}]Coupling["g1e",{a,b}]
+	FF[Vector, {"Zp",0}, {Right,Right}, {a_,b_,i_u,j_u}] :> Coupling["g1u",{i,j}]Coupling["g1e",{a,b}],
+	FF[Vector, {"Zp",0}, {Left,Left},   {a_,b_,i_d,j_d}] :> Coupling["g1q",{i,j}]Coupling["g1l",{a,b}],
+	FF[Vector, {"Zp",0}, {Left,Right},  {a_,b_,i_d,j_d}] :> Coupling["g1d",{i,j}]Coupling["g1l",{a,b}],
+	FF[Vector, {"Zp",0}, {Right,Left},  {a_,b_,i_d,j_d}] :> Coupling["g1q",{i,j}]Coupling["g1e",{a,b}],
+	FF[Vector, {"Zp",0}, {Right,Right}, {a_,b_,i_d,j_d}] :> Coupling["g1d",{i,j}]Coupling["g1e",{a,b}]
 }
 
 
-SubstitutionRulesMediators["Zt"]:={}
+(*SubstitutionRulesMediators["Zt"]:={}*)
 
 
-SubstitutionRulesMediators["Wp"]:={}
+SubstitutionRulesMediators["Wp"]:={
+	(* Vector *)
+	(* NC *)
+	FF[Vector, {"Wp",0}, {Left,Left}, {a_,b_,i_u,j_u}] :> -Coupling["g1q",{i,j}]Coupling["g1l",{a,b}],
+	FF[Vector, {"Wp",0}, {Left,Left}, {a_,b_,i_d,j_d}] :> Coupling["g1q",{i,j}]Coupling["g1l",{a,b}],
+	(* CC *)
+	FF[Vector, {"Wp",0}, {Left,Left}, {a_,b_,i_u,j_d}] :> 2 Coupling["g1q",{i,j}]Coupling["g1l",{a,b}]
+}
 
 
 SubstitutionRulesMediators["S1"]:={
-	(* Vector *)
-	(* NC *)
-	FF[Vector, {"S1",0}, {Left,Right}, {a_,b_,i_u,j_u}] :> -(1/2)Coupling["y1L",{j,b}]Coupling["y1L",{i,a}]\[Conjugate],
-	FF[Vector, {"S1",0}, {Right,Left}, {a_,b_,i_u,j_u}] :> -(1/2)Coupling["y1R",{j,b}]Coupling["y1R",{i,a}]\[Conjugate],
-	(* CC *)
-	FF[Vector, {"S1",0}, {Left,Right}, {a_,b_,i_u,j_d}] :> 1/2 Coupling["y1L",{j,b}]Coupling["y1L",{i,a}]\[Conjugate],
-	FF[Vector, {"S1",0}, {Left,Right}, {a_,b_,i_d,j_u}] :> 1/2 Coupling["y1L",{i,a}]\[Conjugate]Coupling["y1L",{j,b}],
-	FF[Vector, {"S1",0}, {Right,Left}, {a_,b_,i_u,j_d}] :> -(1/2)Coupling["y1Rb",{j,b}]Coupling["y1R",{i,a}]\[Conjugate],
-	FF[Vector, {"S1",0}, {Right,Left}, {a_,b_,i_d,j_u}] :> -(1/2)Coupling["y1Rb",{i,a}]\[Conjugate]Coupling["y1R",{j,b}],
-	
 	(* Scalar *)
 	(* NC *)
 	FF[Scalar, {"S1",0}, {Left,Left}, {a_,b_,i_u,j_u}] :> 1/2 Coupling["y1L",{j,b}]Coupling["y1R",{i,a}]\[Conjugate],
-	FF[Scalar, {"S1",0}, {Right,Right}, {a_,b_,i_u,j_u}] :> 1/2 Coupling["y1L",{i,a}]\[Conjugate]Coupling["y1R",{j,b}],
 	(* CC *)
-	FF[Scalar, {"S1",0}, {Left,Right}, {a_,b_,i_u,j_d}] :> -(1/2)Coupling["y1L",{j,b}]Coupling["y1R",{i,a}]\[Conjugate],
-	FF[Scalar, {"S1",0}, {Right,Left}, {a_,b_,i_d,j_u}] :> -(1/2)Coupling["y1L",{i,a}]\[Conjugate]Coupling["y1R",{j,b}],
+	FF[Scalar, {"S1",0}, {Left,Left}, {a_,b_,i_u,j_d}] :> -(1/2)Coupling["y1L",{j,b}]Coupling["y1R",{i,a}]\[Conjugate],
+	
+	(* Vector *)
+	(* NC *)
+	FF[Vector, {"S1",0}, {Left,Left}, {a_,b_,i_u,j_u}] :> -(1/2)Coupling["y1L",{j,b}]Coupling["y1L",{i,a}]\[Conjugate],
+	FF[Vector, {"S1",0}, {Right,Right}, {a_,b_,i_u,j_u}] :> -(1/2)Coupling["y1R",{j,b}]Coupling["y1R",{i,a}]\[Conjugate],
+	(* CC *)
+	FF[Vector, {"S1",0}, {Left,Left}, {a_,b_,i_u,j_d}] :> 1/2 Coupling["y1L",{j,b}]Coupling["y1L",{i,a}]\[Conjugate],
 	
 	(* Tensor *)
 	(* NC *)
 	FF[Tensor, {"S1",0}, {Left,Left}, {a_,b_,i_u,j_u}] :> -(1/8)Coupling["y1L",{j,b}]Coupling["y1R",{i,a}]\[Conjugate],
-	FF[Tensor, {"S1",0}, {Right,Right}, {a_,b_,i_u,j_u}] :> -(1/8)Coupling["y1L",{i,a}]\[Conjugate]Coupling["y1R",{j,b}],
 	(* CC *)
-	FF[Tensor, {"S1",0}, {Left,Right}, {a_,b_,i_u,j_d}] :> 1/8 Coupling["y1L",{j,b}]Coupling["y1R",{i,a}]\[Conjugate],
-	FF[Tensor, {"S1",0}, {Right,Left}, {a_,b_,i_d,j_u}] :> 1/8 Coupling["y1L",{i,a}]\[Conjugate]Coupling["y1R",{j,b}]
+	FF[Tensor, {"S1",0}, {Left,Left}, {a_,b_,i_u,j_d}] :> 1/8 Coupling["y1L",{j,b}]Coupling["y1R",{i,a}]\[Conjugate]
 }
 
 
-SubstitutionRulesMediators["S1t"]:={}
+SubstitutionRulesMediators["S1t"]:={
+	(* Vector *)
+	(* NC *)
+	FF[Vector, {"S1t",0}, {Right,Right}, {a_,b_,i_d,j_d}] :> 1/2 Coupling["y1Rt",{j,b}]Coupling["y1Rt",{i,a}]\[Conjugate]
+}
 
 
 SubstitutionRulesMediators["U1"]:={
+	(* Scalar *)
+	(* NC *)
+	FF[Scalar,{"U1",0}, {Left,Right},   {a_,b_,i_d,j_d}] :> -2 Coupling["x1L",{i,b}]Coupling["x1R",{j,a}]\[Conjugate],
+	(* CC *)
+	FF[Scalar,{"U1",0}, {Left,Right},   {a_,b_,i_u,j_d}] :> -2 Coupling["x1L",{i,b}]Coupling["x1R",{j,a}]\[Conjugate],
+	
 	(* Vector *)
 	(* NC *)
 	FF[Vector, {"U1",0}, {Left,Left},   {a_,b_,i_d,j_d}] :> Coupling["x1L",{j,a}]\[Conjugate]Coupling["x1L",{i,b}],
 	FF[Vector, {"U1",0}, {Right,Right}, {a_,b_,i_d,j_d}] :> Coupling["x1R",{j,a}]\[Conjugate]Coupling["x1R",{i,b}],
 	(* CC *)
-	FF[Vector, {"U1",0}, {Left,Left},   {a_,b_,i_u,j_d}] :> Coupling["x1L",{j,a}]\[Conjugate]Coupling["x1L",{i,b}],
-	FF[Vector, {"U1",0}, {Left,Left},   {a_,b_,i_d,j_u}] :> Coupling["x1L",{i,b}]Coupling["x1L",{j,a}]\[Conjugate],
-	FF[Vector, {"U1",0}, {Right,Right}, {a_,b_,i_u,j_d}] :> Coupling["x1R",{j,a}]\[Conjugate]Coupling["x1Rb",{i,b}],
-	FF[Vector, {"U1",0}, {Right,Right}, {a_,b_,i_d,j_u}] :> Coupling["x1R",{i,b}]Coupling["x1Rb",{j,a}]\[Conjugate],
-	
-	(* Scalar *)
-	(* NC *)
-	FF[Scalar,{"U1",0}, {Right,Left},   {a_,b_,i_d,j_d}] :> -2 Coupling["x1L",{j,a}]\[Conjugate]Coupling["x1R",{i,b}],
-	FF[Scalar,{"U1",0}, {Left,Right},   {a_,b_,i_d,j_d}] :> -2 Coupling["x1L",{i,b}]Coupling["x1R",{j,a}]\[Conjugate],
-	(* CC *)
-	FF[Scalar,{"U1",0}, {Right,Left},   {a_,b_,i_u,j_d}] :> -2 Coupling["x1L",{j,a}]\[Conjugate]Coupling["x1Rb",{i,b}],
-	FF[Scalar,{"U1",0}, {Left,Right},   {a_,b_,i_d,j_u}] :> -2 Coupling["x1L",{i,b}]Coupling["x1Rb",{j,a}]\[Conjugate]
+	FF[Vector, {"U1",0}, {Left,Left},   {a_,b_,i_u,j_d}] :> Coupling["x1L",{j,a}]\[Conjugate]Coupling["x1L",{i,b}]
 }
 
 
-SubstitutionRulesMediators["U1t"]:={}
+SubstitutionRulesMediators["U1t"]:={
+	(* Vector *)
+	(* NC *)
+	FF[Vector, {"U1t",0}, {Right,Right}, {a_,b_,i_u,j_u}] :> Coupling["x1Rt",{i,b}]Coupling["x1Rt",{j,a}]\[Conjugate]
+}
 
 
 SubstitutionRulesMediators["R2"]:={
-	(* Vector *)
-	(* NC *)
-	FF[Vector, {"R2",0}, {Right,Left},  {a_,b_,i_u,j_u}] :> 1/2 Coupling["y2R",{j,a}]\[Conjugate]Coupling["y2R",{i,b}],
-	FF[Vector, {"R2",0}, {Left,Right},  {a_,b_,i_u,j_u}] :> 1/2 Coupling["y2L",{j,a}]\[Conjugate]Coupling["y2L",{i,b}],
-	FF[Vector, {"R2",0}, {Right,Left},  {a_,b_,i_d,j_d}] :> 1/2 Coupling["y2R",{j,a}]\[Conjugate]Coupling["y2R",{i,b}],
-	
 	(* Scalar *)
 	(* NC *)
-	FF[Scalar, {"R2",0}, {Left,Left},   {a_,b_,i_u,j_u}] :> 1/2 Coupling["y2R",{j,a}]\[Conjugate]Coupling["y2L",{i,b}],
-	FF[Scalar, {"R2",0}, {Right,Right}, {a_,b_,i_u,j_u}] :> 1/2 Coupling["y2R",{i,b}]Coupling["y2L",{j,a}]\[Conjugate],
+	FF[Scalar, {"R2",0}, {Left,Left},   {a_,b_,i_u,j_u}] :> -(1/2)Coupling["y2R",{j,a}]\[Conjugate]Coupling["y2L",{i,b}],
 	(* CC *)
-	FF[Scalar, {"R2",0}, {Left,Left},   {a_,b_,i_u,j_d}] :> -(1/2)Coupling["y2R",{j,a}]\[Conjugate]Coupling["y2L",{i,b}],
-	FF[Scalar, {"R2",0}, {Right,Right}, {a_,b_,i_d,j_u}] :> -(1/2)Coupling["y2R",{i,b}]Coupling["y2L",{j,a}],
+	FF[Scalar, {"R2",0}, {Left,Left},   {a_,b_,i_u,j_d}] :> 1/2 Coupling["y2R",{j,a}]\[Conjugate]Coupling["y2L",{i,b}],
+
+	(* Vector *)
+	(* NC *)
+	FF[Vector, {"R2",0}, {Left,Right},  {a_,b_,i_u,j_u}] :> 1/2 Coupling["y2L",{j,a}]\[Conjugate]Coupling["y2L",{i,b}],
+	FF[Vector, {"R2",0}, {Right,Left},  {a_,b_,i_u,j_u}] :> 1/2 Coupling["y2R",{j,a}]\[Conjugate]Coupling["y2R",{i,b}],
+	FF[Vector, {"R2",0}, {Right,Left},  {a_,b_,i_d,j_d}] :> 1/2 Coupling["y2R",{j,a}]\[Conjugate]Coupling["y2R",{i,b}],
 	
 	(* Tensor *)
 	(* NC *)
-	FF[Tensor, {"R2",0}, {Left,Left},   {a_,b_,i_u,j_u}] :> 1/8 Coupling["y2R",{j,a}]\[Conjugate]Coupling["y2L",{i,b}],
-	FF[Tensor, {"R2",0}, {Right,Right}, {a_,b_,i_u,j_u}] :> 1/8 Coupling["y2R",{i,b}]Coupling["y2L",{j,a}]\[Conjugate],
+	FF[Tensor, {"R2",0}, {Left,Left},   {a_,b_,i_u,j_u}] :> -(1/8)Coupling["y2R",{j,a}]\[Conjugate]Coupling["y2L",{i,b}],
 	(* CC *)
-	FF[Tensor, {"R2",0}, {Left,Left},   {a_,b_,i_u,j_d}] :> -(1/8)Coupling["y2R",{j,a}]\[Conjugate]Coupling["y2L",{i,b}],
-	FF[Tensor, {"R2",0}, {Right,Right}, {a_,b_,i_d,j_u}] :> -(1/8)Coupling["y2R",{i,b}]Coupling["y2L",{j,a}]
+	FF[Tensor, {"R2",0}, {Left,Left},   {a_,b_,i_u,j_d}] :> 1/8 Coupling["y2R",{j,a}]\[Conjugate]Coupling["y2L",{i,b}]
 }
 
 
-SubstitutionRulesMediators["R2t"]:={}
+SubstitutionRulesMediators["R2t"]:={
+	(* Vector *)
+	(* NC *)
+	FF[Vector, {"R2t",0}, {Left,Right}, {a_,b_,i_d,j_d}] :> 1/2 Coupling["y2Lt",{i,b}]Coupling["y2Lt",{j,a}]\[Conjugate]
+}
 
 
-SubstitutionRulesMediators["V2"]:={}
+SubstitutionRulesMediators["V2"]:={
+	(* Scalar *)
+	(* NC *)
+	FF[Scalar, {"V2",0}, {Left,Right}, {a_,b_,i_d,j_d}] :> 2 Coupling["x2R",{i,a}]\[Conjugate]Coupling["x2L",{j,b}],
+	(* CC *)
+	FF[Scalar, {"V2",0}, {Left,Right}, {a_,b_,i_u,j_d}] :> 2 Coupling["x2R",{i,a}]\[Conjugate]Coupling["x2L",{j,b}],
+	
+	(* Vector *)
+	(* NC *)
+	FF[Vector, {"V2",0}, {Right,Left}, {a_,b_,i_u,j_u}] :> - Coupling["x2R",{i,a}]\[Conjugate]Coupling["x2R",{j,b}],
+	FF[Vector, {"V2",0}, {Left,Right}, {a_,b_,i_d,j_d}] :> - Coupling["x2L",{i,a}]\[Conjugate]Coupling["x2L",{j,b}],
+	FF[Vector, {"V2",0}, {Left,Right}, {a_,b_,i_d,j_d}] :> - Coupling["x2R",{i,a}]\[Conjugate]Coupling["x2R",{j,b}]
+}
 
 
-SubstitutionRulesMediators["V2t"]:={}
+SubstitutionRulesMediators["V2t"]:={
+	(* Vector *)
+	(* NC *)
+	FF[Vector, {"V2t",0}, {Left,Right}, {a_,b_,i_u,j_u}] :> Coupling["x2Lt",{i,a}]\[Conjugate]Coupling["x2Lt",{j,b}]
+}
 
 
-SubstitutionRulesMediators["S3"]:={}
+SubstitutionRulesMediators["S3"]:={
+	(* Vector *)
+	(* NC *)
+	FF[Vector, {"S3",0}, {Left,Left}, {a_,b_,i_u,j_u}] :> -(1/2)Coupling["y3L",{j,b}]Coupling["y3L",{i,a}]\[Conjugate],
+	FF[Vector, {"S3",0}, {Left,Left}, {a_,b_,i_d,j_d}] :> -Coupling["y3L",{j,b}]Coupling["y3L",{i,a}]\[Conjugate],
+	(* CC *)
+	FF[Vector, {"S3",0}, {Left,Left}, {a_,b_,i_u,j_d}] :> -(1/2)Coupling["y3L",{j,b}]Coupling["y3L",{i,a}]\[Conjugate]
+}
 
 
-SubstitutionRulesMediators["U3"]:={}
+SubstitutionRulesMediators["U3"]:={
+	(* Vector *)
+	(* NC *)
+	FF[Vector, {"U3",0}, {Left,Left}, {a_,b_,i_u,j_u}] :> 2 Coupling["x3L",{i,b}]Coupling["x3L",{j,a}]\[Conjugate],
+	FF[Vector, {"U3",0}, {Left,Left}, {a_,b_,i_d,j_d}] :> Coupling["x3L",{i,b}]Coupling["x3L",{j,a}]\[Conjugate],
+	(* CC *)
+	FF[Vector, {"U3",0}, {Left,Left}, {a_,b_,i_u,j_d}] :> -Coupling["x3L",{i,b}]Coupling["x3L",{j,a}]\[Conjugate]
+}
