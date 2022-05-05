fabe20d443dd83e50197188d55ab57c40331e5a7
Lukas Allwicher <lukall@physik.uzh.ch>
2022-05-05 10:27:27 +0200

    defined structure for low-energy part

---
+++ HighPT/Flavor/Template.m
@@ -0,0 +1,39 @@
+(* ::Package:: *)
+
+Package["HighPT`"]
+
+
+(* ::Title:: *)
+(*HighPT``*)
+
+
+(* ::Subtitle:: *)
+(*File description*)
+
+
+(* ::Chapter:: *)
+(*Public:*)
+
+
+(* ::Section:: *)
+(*Scoping*)
+
+
+(* ::Subsection:: *)
+(*Exported*)
+
+
+(* ::Subsection:: *)
+(*Internal*)
+
+
+(* ::Chapter:: *)
+(*Private:*)
+
+
+(* ::Section:: *)
+(*Section1*)
+
+
+(* ::Subsection:: *)
+(*Subsection 1.1*)
