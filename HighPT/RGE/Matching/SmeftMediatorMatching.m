fb3b4763b6d66906afceefb63ac2ea2fbbfb53b7
Lukas Allwicher <lukall@physik.uzh.ch>
2023-06-01 14:22:45 -0700

    started working on SmeftMediatorMatching

--- HighPT/RGE/Matching/SmeftMediatorMatching.m
+++ HighPT/RGE/Matching/SmeftMediatorMatching.m
@@ -4,11 +4,11 @@ Package["HighPT`"]
 
 
 (* ::Title:: *)
-(*HighPT`Template`*)
+(*HighPT`SmeftMediatorMatching`*)
 
 
 (* ::Subtitle:: *)
-(*Template .m file*)
+(*Match the NP mediators to the SMEFT at tree-level and dimension 6*)
 
 
 (* ::Chapter:: *)
@@ -27,5 +27,30 @@ Package["HighPT`"]
 (*Internal*)
 
 
+PackageScope["MatchMediator"]
+
+
 (* ::Chapter:: *)
 (*Private:*)
+
+
+MediatorMatchingConditions= <|
+	"S1"->{},
+	"S1t"->{},
+	"U1"->{	
+		WC["lq1",{\[Alpha]_,\[Beta]_,i_,j_}]->-(1/2)Coupling["x1L",{i,\[Beta]}]Coupling["x1L",{j,\[Alpha]}]\[Conjugate],
+		WC["lq3",{\[Alpha]_,\[Beta]_,i_,j_}]->-(1/2)Coupling["x1L",{i,\[Beta]}]Coupling["x1L",{j,\[Alpha]}]\[Conjugate],
+		WC["ed",{\[Alpha]_,\[Beta]_,i_,j_}]->-Coupling["x1R",{i,\[Beta]}]Coupling["x1R",{j,\[Alpha]}]\[Conjugate],
+		WC["ledq",{\[Alpha]_,\[Beta]_,i_,j_}]->2Coupling["x1R",{j,\[Beta]}]Coupling["x1L",{i,\[Alpha]}]\[Conjugate]
+		},
+	"U1t"->{},
+	"R2"->{},
+	"R2t"->{},
+	"V2"->{},
+	"V2t"->{},
+	"S3"->{},
+	"U3"->{}
+|>
+
+
+MatchMediator[mediator_]:=MediatorMatchingConditions[mediator]
